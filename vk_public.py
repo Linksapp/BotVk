ce09d1fa6ed01e15d673e0a91cfa00aafe189a15
Linksapp <nikitindima1500@gmail.com>
2022-03-06 22:14:16 +0300

    Добавлен config и user

--- vk_public.py
+++ vk_public.py
@@ -1,11 +1,8 @@
-from cmath import inf
-from dataclasses import field
-from operator import ge
-from webbrowser import get
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
+from user import User
+from config import *
 
-token = '5f4851e6030f4d498493b024950c837cf1ce8ed6a5c21eef1ec235e877578a654480b58918442f4682388'
 session = vk_api.VkApi(token=token)
 vk = session.get_api()
 longpoll = VkLongPoll(session, group_id=211021014)
@@ -37,7 +34,6 @@ def get_info_about_member(id):
 	info['last_name'] = vk.users.get(user_id = id)[0]['last_name']
 	return info
 
-print(get_info_about_member(208156464))
 
 
 for event in longpoll.listen():
