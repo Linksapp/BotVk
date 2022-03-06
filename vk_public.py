b4f1cf2d1976645dc87d6b4e831d181313591ce5
Linksapp <nikitindima1500@gmail.com>
2022-03-06 21:06:00 +0300

    Добавлена функция send_message

--- vk_public.py
+++ vk_public.py
@@ -1,21 +1,32 @@
 import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType    
+from vk_api.longpoll import VkLongPoll, VkEventType
 
 token = '5f4851e6030f4d498493b024950c837cf1ce8ed6a5c21eef1ec235e877578a654480b58918442f4682388'
 session = vk_api.VkApi(token=token)
 vk = session.get_api()
 longpoll = VkLongPoll(session, group_id=211021014)
 
-#print(members)
+
 def registration(id):
+	global members
 	members = vk.groups.getMembers(group_id=211021014)['items']
 	return id in members
-	
+
+def send_message(id, text):
+	print(type(id))
+	if isinstance(id, list):
+		print('list')
+		for _ in id:
+			vk.messages.send(user_id=_, random_id=0, message=text)
+	elif isinstance(id, int):
+		vk.messages.send(user_id=id, random_id=0, message=text)
+
 for event in longpoll.listen():
 	if event.type == VkEventType.MESSAGE_NEW and registration(event.user_id) and event.to_me:
-		vk.messages.send(user_id=event.user_id, random_id=0, message='Hello')
-		
+		send_message(event.user_id,'Hello')
+		# send_message(members, 'Рассылка подписчикам')
 
-# USER_RECORDIN_VOICE
+
+# USER_RECORDING_VOICE
 
 
