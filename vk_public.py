11a899c26bf884ea5ba90be757c7504d1a96c37a
Linksapp <nikitindima1500@gmail.com>
2022-03-06 22:07:57 +0300

    Функция по сбору информации

--- vk_public.py
+++ vk_public.py
@@ -1,3 +1,7 @@
+from cmath import inf
+from dataclasses import field
+from operator import ge
+from webbrowser import get
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
@@ -8,22 +12,38 @@ longpoll = VkLongPoll(session, group_id=211021014)
 
 
 def registration(id):
+	'''
+	Проверяет подписку на группу
+	'''
 	global members
 	members = vk.groups.getMembers(group_id=211021014)['items']
 	return id in members
 
 def send_message(id, text):
-	print(type(id))
+	'''
+	
+	'''
 	if isinstance(id, list):
-		print('list')
 		for _ in id:
-			vk.messages.send(user_id=_, random_id=0, message=text)
+			vk.messages.send(user_id=_, random_id=0, message=str(text))
 	elif isinstance(id, int):
-		vk.messages.send(user_id=id, random_id=0, message=text)
+		vk.messages.send(user_id=id, random_id=0, message=str(text))
+
+def get_info_about_member(id):
+	info = {}
+	# person_id = str(vk.users.get(user_ids = id)[0]['id'])
+	info['id'] = vk.users.get(user_id = id)[0]['id']
+	info['first_name'] = vk.users.get(user_id = id)[0]['first_name']
+	info['last_name'] = vk.users.get(user_id = id)[0]['last_name']
+	return info
+
+print(get_info_about_member(208156464))
+
 
 for event in longpoll.listen():
 	if event.type == VkEventType.MESSAGE_NEW and registration(event.user_id) and event.to_me:
 		send_message(event.user_id,'Hello')
+		# send_message(event.user_id, get_info_about_member(event.user_id))
 		# send_message(members, 'Рассылка подписчикам')
 
 
