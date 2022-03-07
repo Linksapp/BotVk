e50953f4a481ec16614a167376f043cb85935448
Tresian <ddrozzev5@gmail.com>
2022-03-07 14:26:57 +0300

    Код ревью, функция create_user

--- user.py
+++ user.py
@@ -4,17 +4,28 @@ class User:
     """Default User Class"""
     def __init__(self, user_info: dict, registration: bool = False) -> None:
         # Пользовательские данные
-        self.user_id = user_info['id']
-        self.user_name = [user_info['first_name'], user_info['last_name']]
-        self.user_domain = user_info['domain']
+        if user_info == {} or len(user_info) < 4:
+            self.create = False
+            self.return_error()
+        else:
+            self.user_id = user_info['id']
+            self.user_name = [user_info['first_name'], user_info['last_name']]
+            self.user_domain = user_info['domain']
 
-        self.registratiom = registration
+            self.registratiom = registration
+            self.create = True
     
     def get_info(self):
         pass
 
     def save_info(self):
         pass
+
+    def return_error(self): raise 'Error! Dont get user_info like full dict'
+
+    def check_create(self):
+        if self.create: return True
+        else: return False
 
     def print_info(self):
         print(self.user_id,
@@ -30,5 +41,3 @@ a = {
     'last_name': 1,
     'domain': 'domain'
 }
-c = User(a,1)
-c.print_info()
--- vk_public.py
+++ vk_public.py
@@ -1,14 +1,16 @@
+import sys
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from user import User
 from config import *
 
-session = vk_api.VkApi(token=token)
-vk = session.get_api()
-longpoll = VkLongPoll(session, group_id=211021014)
 
+def create_user(registration: bool) -> bool:
+	user = User(get_info_about_user(id), registration)
+	if user.check_create(): return True
+	else: return False
 
-def registration(id):
+def registration(id: int) -> bool:
 	'''
 	Проверяет подписку на группу
 	'''
@@ -16,7 +18,7 @@ def registration(id):
 	members = vk.groups.getMembers(group_id=211021014)['items']
 	return id in members
 
-def send_message(id, text):
+def send_message(id: int | list, text: str):
 	'''
 	Отправляет сообщение
 	'''
@@ -26,28 +28,42 @@ def send_message(id, text):
 	elif isinstance(id, int):
 		vk.messages.send(user_id=id, random_id=0, message=str(text))
 
-def get_info_about_user(id):
+def get_info_about_user(id: int) -> dict:
 	'''
 	Возвращает информацию о пользователе 
 	'''
 	info = {}
-	info['id'] = vk.users.get(user_id = id)[0]['id']
-	info['first_name'] = vk.users.get(user_id = id)[0]['first_name']
-	info['last_name'] = vk.users.get(user_id = id)[0]['last_name']
-	return info
+	try:
+		info['id'] = vk.users.get(user_id = id)[0]['id']
+		info['first_name'] = vk.users.get(user_id = id)[0]['first_name']
+		info['last_name'] = vk.users.get(user_id = id)[0]['last_name']
+		return info
+	except Exception as error:
+		print(error)
+		return info
 
 
+def bot_cycle():
+	for event in longpoll.listen():
+		text = event.text.lower()
+		if text == 'начать' and event.type == VkEventType.MESSAGE_NEW and event.to_me:
+			if registration(event.user_id): 
+				send_message(event.user_id,'Hello')
+				create_user(True)
+			# send_message(event.user_id, get_info_about_user(event.user_id))
+			# send_message(members, 'Рассылка подписчикам')
+			elif registration(event.user_id) == False: 
+				send_message(event.user_id, 'Подпишись на группу!')
+				create_user(False)
+		if event.user_id in admin_ids and event.type == VkEventType.MESSAGE_NEW:
+			if text == 'exit()': sys.exit()
 
-for event in longpoll.listen():
-	text = event.text.lower()
-	if text == 'начать':
-		if event.type == VkEventType.MESSAGE_NEW and registration(event.user_id) and event.to_me:
-			send_message(event.user_id,'Hello')
-		# send_message(event.user_id, get_info_about_user(event.user_id))
-		# send_message(members, 'Рассылка подписчикам')
-		elif event.type == VkEventType.MESSAGE_NEW and registration(event.user_id) == False and event.to_me:
-			send_message(event.user_id, 'Подпишись на группу')
-
-# USER_RECORDING_VOICE
+	# USER_RECORDING_VOICE
 
 
+if __name__ == "__main__":
+	session = vk_api.VkApi(token=token)
+	vk = session.get_api()
+
+	longpoll = VkLongPoll(session, group_id=211021014)
+	bot_cycle()
