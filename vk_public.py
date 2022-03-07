9801e2b0229ffa1ef924250cb5a0007eb0800db6
Tresian <ddrozzev5@gmail.com>
2022-03-07 14:39:57 +0300

    fixed the bag with text

--- vk_public.py
+++ vk_public.py
@@ -6,22 +6,19 @@ from config import *
 
 
 def create_user(registration: bool) -> bool:
+	"""Создает пользователя"""
 	user = User(get_info_about_user(id), registration)
 	if user.check_create(): return True
 	else: return False
 
 def registration(id: int) -> bool:
-	'''
-	Проверяет подписку на группу
-	'''
+	"""Проверяет подписку"""
 	global members
 	members = vk.groups.getMembers(group_id=211021014)['items']
 	return id in members
 
 def send_message(id: int | list, text: str):
-	'''
-	Отправляет сообщение
-	'''
+	"""Отправляет сообщения"""
 	if isinstance(id, list):
 		for _ in id:
 			vk.messages.send(user_id=_, random_id=0, message=str(text))
@@ -29,9 +26,7 @@ def send_message(id: int | list, text: str):
 		vk.messages.send(user_id=id, random_id=0, message=str(text))
 
 def get_info_about_user(id: int) -> dict:
-	'''
-	Возвращает информацию о пользователе 
-	'''
+	"""Возвращает информацию о пользователе"""
 	info = {}
 	try:
 		info['id'] = vk.users.get(user_id = id)[0]['id']
@@ -44,22 +39,24 @@ def get_info_about_user(id: int) -> dict:
 
 
 def bot_cycle():
+	"""Longpoll цикл бота"""
+	# USER_RECORDING_VOICE
+	# send_message(event.user_id, get_info_about_user(event.user_id))
+	# send_message(members, 'Рассылка подписчикам')
 	for event in longpoll.listen():
 		if event.type == VkEventType.MESSAGE_NEW and event.to_me:
 			text = event.text.lower()
 			if text == 'начать':
 				if registration(event.user_id): 
 					send_message(event.user_id,'Hello')
-					create_user(True)
-				# send_message(event.user_id, get_info_about_user(event.user_id))
-				# send_message(members, 'Рассылка подписчикам')
+					create_user(True)			
 				elif registration(event.user_id) == False: 
 					send_message(event.user_id, 'Подпишись на группу!')
 					create_user(False)
 			if event.user_id in admin_ids:
 				if text == 'exit()': sys.exit()
 
-	# USER_RECORDING_VOICE
+	
 
 
 if __name__ == "__main__":
