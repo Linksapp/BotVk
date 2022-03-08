b4b95a30f9e7299c24e4c5f84e2ec754481fade0
Tresian <ddrozzev5@gmail.com>
2022-03-08 11:09:04 +0300

    cod rewie

--- user.py
+++ user.py
@@ -4,9 +4,7 @@ class User:
     """Default User Class"""
     def __init__(self, user_info: dict, registration: bool = False) -> None:
         # Пользовательские данные
-        if user_info == {}:
-            self.create = False
-            self.return_error()
+        if user_info == {}: self.create = False
         else:
             self.user_id = user_info['id']
             self.user_name = [user_info['first_name'], user_info['last_name']]
@@ -14,23 +12,29 @@ class User:
 
             self.registratiom = registration
             self.create = True
+
+        self.return_error(self.create)
+        self.save_info()
     
     def get_info(self):
         pass
 
     def save_info(self):
-        with open('dumb_db.txt', 'w') as f:
+        with open('dumb_db.txt', 'w', encoding='UTF-8') as f:
             f.write(f'{self.user_id} - {self.user_name}')
             return True
     
-    def return_error(self): raise BaseException('Error! Dont get user_info like full dict')
+    def return_error(self, create: bool): 
+        if create == False: raise BaseException('Error! Dont get user_info like full dict')
+        else: pass
 
-    def check_create(self):
-        with open('dumb_db.txt') as f:
+    def check_create(self) -> bool:
+        """Дописать функцию"""
+        with open('dumb_db.txt', 'r', encoding='UTF-8') as f:
             if self.user_id in f:
                 return True
             else:
-                User.save_info(self)
+                self.save_info() # передовать все необходимые аргументы для сохранения
                 return True
 
     def print_info(self):
--- vk_public.py
+++ vk_public.py
@@ -5,9 +5,9 @@ from user import User
 from config import *
 #
 
-def create_user(id: int) -> bool:
+def create_user(id: int, registration: bool) -> bool:
 	"""Создает пользователя"""
-	user = User(get_info_about_user(id))
+	user = User(get_info_about_user(id), registration)
 	if user.check_create(): return True
 	else: return False
 
@@ -15,24 +15,24 @@ def registration(id: int) -> bool:
 	"""Проверяет подписку"""
 	global members
 	members = vk.groups.getMembers(group_id=211021014)['items']
-	if id in members: return True
-	else: return False
-
+	return id in members
+	
 def send_message(id: int | list, text: str):
 	"""Отправляет сообщения"""
 	if isinstance(id, list):
-		for _ in id:
-			vk.messages.send(user_id=_, random_id=0, message=str(text))
-	elif isinstance(id, int):
-		vk.messages.send(user_id=id, random_id=0, message=str(text))
+		for _ in id: vk.messages.send(user_id=_, random_id=0, message=str(text))
+	elif isinstance(id, int): vk.messages.send(user_id=id, random_id=0, message=str(text))
 
 def get_info_about_user(id: int) -> dict:
 	"""Возвращает информацию о пользователе"""
 	info = {}
+	fields = ['id', 'first_name', 'last_name', 'domain']
 	try:
-		info['id'] = vk.users.get(user_id = id)[0]['id']
+		for _ in fields: info[_] = vk.users.get(user_id = id)[0][_]
+
+		"""info['id'] = vk.users.get(user_id = id)[0]['id']
 		info['first_name'] = vk.users.get(user_id = id)[0]['first_name']
-		info['last_name'] = vk.users.get(user_id = id)[0]['last_name']
+		info['last_name'] = vk.users.get(user_id = id)[0]['last_name']"""
 		return info
 	except Exception as error:
 		print(error)
@@ -41,24 +41,24 @@ def get_info_about_user(id: int) -> dict:
 
 def bot_cycle():
 	"""Longpoll цикл бота"""
-	for event in longpoll.listen():
-		if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-			text = event.text.lower()
-			if text == 'начать':
-				if registration(event.user_id): 
-					send_message(event.user_id,'Hello')	
-					create_user(event.user_id)		
-				elif registration(event.user_id) == False: 
-					send_message(event.user_id, 'Подпишись на группу!')		# будет проверять только если введено "начать", надо или сбрасывать кнопку, или чтобы приветствие всегда было первым сообщением
-			if event.user_id in admin_ids:			# убрал добавление не подписчика в базу
-				if text == 'exit()': sys.exit()
+	while True:
+		for event in longpoll.listen():
+			if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+				text = event.text.lower()
+				if text == 'начать':
+					if registration(event.user_id): 
+						send_message(event.user_id,'Hello')	
+						create_user(event.user_id, True)		
+					elif registration(event.user_id) == False: 
+						send_message(event.user_id, 'Подпишись на группу!')		# будет проверять только если введено "начать", надо или сбрасывать кнопку, или чтобы приветствие всегда было первым сообщением
+				if event.user_id in admin_ids:			# убрал добавление не подписчика в базу
+					if text == 'exit()': sys.exit()
 
-	
 
 
 if __name__ == "__main__":
 	session = vk_api.VkApi(token=token)
 	vk = session.get_api()
 
-	longpoll = VkLongPoll(session, group_id=211021014)
+	longpoll = VkLongPoll(session, group_id=group_id)
 	bot_cycle()
