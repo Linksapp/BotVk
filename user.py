6ac07cf0883273db40dae4ba9d1756ad6ce02374
Linksapp <nikitindima1500@gmail.com>
2022-03-09 20:46:39 +0300

    fixed save_info and check_greate (i guess)

--- user.py
+++ user.py
@@ -1,44 +1,48 @@
 import json
 #
+
+"""Переделать класс, создать модуль для записи информации в бд"""
+
 class User:
     """Default User Class"""
     def __init__(self, user_info: dict, registration: bool = False) -> None:
         # Пользовательские данные
-        if user_info == {}:
-            self.create = False
-            self.return_error()
+        if user_info == {}: self.create = False
         else:
-            self.user_id = user_info['id']
-            self.user_name = [user_info['first_name'], user_info['last_name']]
-            # self.user_domain = user_info['domain']
-
+            self.user_info = user_info
             self.registratiom = registration
             self.create = True
+
+        # self.return_error()  # закоментил, надо куда-то это сунуть или ещё раз объясни почему именно здесь стоит.
+        # self.save_info()
     
     def get_info(self):
         pass
 
     def save_info(self):
-        with open('dumb_db.txt', 'w') as f:
-            f.write(f'{self.user_id} - {self.user_name}')
+        """При записи нового пользователя, очищает бд. Фикс it"""
+        # пофиксил вроде
+        with open('dumb_db.txt', 'a', encoding='UTF-8') as file:
+            file.write(str(self.user_info) + '\n')
             return True
     
-    def return_error(self): raise BaseException('Error! Dont get user_info like full dict')
+    def return_error(self): 
+        if self.create == False: raise BaseException('Error! Dont get user_info like full dict')
 
-    def check_create(self):
-        with open('dumb_db.txt') as f:
-            if self.user_id in f:
+    def check_create(self) -> bool:
+        """Переделать функцию"""
+
+        with open('dumb_db.txt', 'r', encoding='UTF-8') as file:
+            data = file.read()
+            if str(self.user_info['id']) in data:
                 return True
             else:
-                User.save_info(self)
+                self.save_info()
                 return True
 
     def print_info(self):
-        print(self.user_id,
-        *self.user_name,
-        # self.user_domain,
-        sep = '\n'
-        )
+        print(self.user_info,
+        sep = '\n')
 
 
 a = {
