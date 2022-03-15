4fd52486c5af4bc4b1b62c609e232f5103e585d6
Дмитрий <72344380+Linksapp@users.noreply.github.com>
2022-03-15 19:44:24 +0300

    made some changes with keyboard

--- vk_public.py
+++ vk_public.py
@@ -1,11 +1,12 @@
 import sys, time
+from turtle import color
 import vk_api, database
 from vk_api.longpoll import VkLongPoll, VkEventType
-from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor, VkKeyboardButton
 from user import User
 from config import *
 import json
-#
+
 """Переделать архитектуру записи и чтения данных"""
 
 
@@ -42,15 +43,30 @@ def get_info_about_user(id: int) -> dict:
 		print('Def get_info_about_user', error)
 		return info
 
-def simple_keyboard(one_time: bool = False):
+def create_empty_keyboard(one_time: bool = False):
 	keyboard = VkKeyboard(one_time=one_time)
-	keyboard.add_button('Красная', color=VkKeyboardColor.NEGATIVE)
-	keyboard.add_button('Назад', color=VkKeyboardColor.PRIMARY)
+	return keyboard.get_empty_keyboard()
+
+def create_main_keyboard(one_time: bool = False):
+	keyboard = VkKeyboard(one_time=one_time)
+	keyboard.add_button('Красная', color='negative')
+	keyboard.add_button('Зелёная', color='positive')
+	keyboard.add_button('Синяя', color='primary')
+	keyboard.add_button('Белая', color='secondary')
+	keyboard.add_line()
+	keyboard.add_button('Меню 2')		
 	return keyboard.get_keyboard()
 
-def create_empty_keyboard():
-	keyboard = VkKeyboard()
-	return keyboard.get_empty_keyboard()
+def create_menu2_keyboard(one_time: bool = False):
+	keyboard = VkKeyboard(one_time=one_time)
+	keyboard.add_button('Фото', color='negative')
+	keyboard.add_button('Назад')
+	return keyboard.get_keyboard()
+
+def create_start_keyboard(one_time: bool = False):
+	keyboard = VkKeyboard(one_time=one_time)
+	keyboard.add_button('Начать', color='primary')
+	return keyboard.get_keyboard()
 
 def bot_cycle():
 	"""Longpoll цикл бота"""
@@ -60,21 +76,18 @@ def bot_cycle():
 				text = event.text.lower()
 				if text == 'начать':
 					if registration(event.user_id): 
-						keyboard = simple_keyboard()
-						send_message(event.user_id,'Hello', keyboard=keyboard)	
+						send_message(event.user_id,'Hello', keyboard=create_main_keyboard())	
 						create_user(event.user_id, True)		# второй раз проверяет регистрацию
 					elif registration(event.user_id) == False: 
-						send_message(event.user_id, 'Подпишись на группу!')		# будет проверять только если введено "начать", надо или сбрасывать кнопку, или чтобы приветствие всегда было первым сообщением
-				if text == 'красная':
-					keyboard = simple_keyboard()
-					send_message(event.user_id, 'Красная', keyboard=keyboard)		# Нужно найти способ вызывать клавиатуру без сообщений  
-				if text == 'назад':
-					# keyboard = simple_keyboard(True)
-					# send_message(event.user_id, 'Вернулись', keyboard=keyboard)		# Клавиатура не пропадает, тк постоянно видит 'назад'
-					keyboard = create_empty_keyboard()
-					send_message(event.user_id, 'Вернулись', keyboard=keyboard)
-				if event.user_id in admin_ids:			# убрал добавление не подписчика в базу
+						send_message(event.user_id, 'Подпишись на группу!', keyboard=create_start_keyboard())
+				if registration(event.user_id) and text == 'меню 2':
+					send_message(event.user_id, 'Меню номер 2', keyboard=create_menu2_keyboard())
+					if text == 'назад':
+						send_message(event.user_id, 'ххххх')		# не работает. Надо понять как работать в пределах одной менюшки, чтобы нельзя было вызвать что-нибудь из других (например получить реакцию на 'начать' из 'меню2')
+				if event.user_id in admin_ids:		
 					if text == 'exit()': sys.exit()
+
+
 
 
 if __name__ == "__main__":
