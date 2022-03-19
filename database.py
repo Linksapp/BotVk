4c6b9f547dfe14ea9f6fe511161fba4df1c5007e
Дмитрий <89359594+Tresian@users.noreply.github.com>
2022-03-19 15:17:35 +0300

    Add files via upload

--- database.py
+++ database.py
@@ -1,9 +1,20 @@
 import json
 
+# Сделать json file
+
 base = 'dumb_db.txt'
 
-def get_info() -> dict:
-    pass
+def get_info(id: int) -> dict | str:
+    try:
+        with open(base, 'r', encoding='UTF-8') as file:
+            for _ in file.readlines():
+                if str(id) in _: 
+                    return _
+
+    except FileNotFoundError as error: 
+        with open(base, 'w', encoding='UTF-8') as file:
+            print('None')
+            return {}
 
 def save_info(user_info: dict) -> bool:
     with open(base, 'a', encoding='UTF-8') as file:
@@ -21,3 +32,4 @@ def read_info(user_info: dict) -> bool:
             file.write(str(user_info) + '\n')
         return True
 
+print(get_info(376919311))
